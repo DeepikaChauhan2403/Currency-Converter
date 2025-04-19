09b16ecb9fcd6825c1a7c89ecb2180760db8da7d
DeepikaChauhan2403 <124796084+DeepikaChauhan2403@users.noreply.github.com>
2025-04-19 19:35:22 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css"
+import ConveterForm from "./components/ConverterForm";
+
+function App() {
+  return (
+    <div className="currency-converter">
+      <h2 className="converter-title"> Currency Conveter</h2>
+      <ConveterForm />
+    </div>
+  );
+}
+
+export default App;
